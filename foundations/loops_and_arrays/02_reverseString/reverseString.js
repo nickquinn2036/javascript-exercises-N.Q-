454103d42489a4922c16a083104ea0e9338619cd
nickquinn2036 <98188445+nickquinn2036@users.noreply.github.com>
2026-09-21 14:16:02 -0300

    Solve 01, 02

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
+const reverseString = function reverseEntireString(word) {
 
+      return word.split("").reverse().join("");
 };
 
 // Do not edit below this line
