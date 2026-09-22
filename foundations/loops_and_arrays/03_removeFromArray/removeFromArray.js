d45eb911761c50b086353b3bd9f188d330a1c603
nickquinn2036 <98188445+nickquinn2036@users.noreply.github.com>
2026-09-21 22:08:02 -0300

    Solve 03,

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArray, ...itemsToRemove) {
+  let cleanArray = [];
+
+  for (const item of mainArray) {
+    
+    if (itemsToRemove.includes(item)) {
+
+    } else {
+
+      cleanArray.push(item);
+    }
+  }
+
+
+  return cleanArray;
 };
 
 // Do not edit below this line
