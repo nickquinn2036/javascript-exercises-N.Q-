2e58e0a63b41131d2df7933aca2b712431e8bf27
nickquinn2036 <98188445+nickquinn2036@users.noreply.github.com>
2026-09-22 11:13:06 -0300

    Solve 04, 05, AND 06

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    
+    let finalSum = 0;
+    let realStart = Math.min(startNum, endNum);
+    let realEnd = Math.max(startNum, endNum);
+    
+    
+    if (startNum < 0 || endNum < 0 || !Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+  return "ERROR";
+}
 
+
+ for (let i = realStart; i <= realEnd; i++) {
+    finalSum += i;
+
+  }
+
+  return finalSum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
--- foundations/loops_and_arrays/05_leapYears/leapYears.js
+++ foundations/loops_and_arrays/05_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(leapFinder) {
+
+  if (leapFinder % 400 === 0) {
+    return true;
+  }
+
+  if (leapFinder % 100 === 0) {
+    return false;
+  }
+  
+  if (leapFinder % 4 === 0) {
+    return true;
+  }
+  
+  return false;
+
+// return (leapFinder % 400 === 0) || (leapFinder % 4 === 0 && leapFinder % 100 !== 0);
 
 };
-
 // Do not edit below this line
 module.exports = leapYears;
